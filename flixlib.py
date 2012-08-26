e6a0a2083692c1e317ae7031a690044ea21c6f8c
William Wanjohi <wwanjohi@barackobama.com>
2012-08-26 03:27:23 -0500

    create Movie class, config and result functions

--- flixlib.py
+++ flixlib.py
@@ -49,53 +49,67 @@ def write_histories_to_file(netflix, accounts):
         with open('user_%s_hist.json' % name, 'w') as f:
             f.write(json.dumps(full_history))
 
-'''
-1. pull movie titles from google as set
-2. pop titles and make objects with stuff
+class Movie:
+    def __init__(self, netflix, gtitle):
+        self.gtitle = gtitle
+        time.sleep(0.1)
+        matches = netflix.search_titles(gtitle, max_results=1)
+        time.sleep(0.1)
+        details = netflix.get_title(matches['catalog'][0]['id'])
+        for k,v in details['catalog_title'].items():
+            setattr(self, k, v)
+        self.predictions = {}
 
-movielist = { 
-              'tree of life' : {
-                'predicted_rating' : 4.5,
-                'netflix_id' : 'api.netflix.blah'
-              },
-              'field of dreams' : True
-            }
-'''
-
-def find_a_movie(location, netflix, accounts):
+def pick_a_movie(location, netflix, users):
     page = urlopen('%s?near=%s' % (GOOGLE_URL, location))
     soup = BeautifulSoup(page)
-    counter = 0
-    movies = {} # this should be a list
-    for movie in soup.findAll('div', attrs={'class': 'movie'}):
-        title = movie.find('div', attrs={'class': 'name'}).text
-        if title in movies: continue
-        movies[title] = {'distance_rank' : counter}
-        counter += 1
-    for movie in movies:
-        time.sleep(0.1)
-        results = netflix.search_titles(movie, max_results=1)
-        title = netflix.get_title(results['catalog'][0]['id'])
-        movies[movie]['netflix_id'] = title['catalog_title']['id']
-        movies[movie]['average'] = title['catalog_title']['average_rating']
-    for username, creds in accounts.items():
-        print 'parsing %s' % username
-        time.sleep(0.1)
-        user = netflix.get_user(creds['access_token'],
-                                creds['access_token_secret'])
-        all_ids = [movie['netflix_id'] for movie in movies.itervalues()]
+    movies = []
+    print 'finding movies'
+    for divmovie in soup.findAll('div', attrs={'class': 'movie'}):
+        title = divmovie.find('div', attrs={'class': 'name'}).text
+        if title in [movie.gtitle for movie in movies]: continue
+        movies.append(Movie(netflix, title))
+        # sys.stdout.write('.')
+    for user in users:
+        print 'parsing %s' % user.nickname
+        all_ids = [movie.id for movie in movies]
         ratings = []
         for i in range(0, len(all_ids), MAX_RESULTS):
             time.sleep(0.1)
-            partratings = user.get_rating(
-                    all_ids[i:i+MAX_RESULTS]
-                )['ratings']
+            partratings = user.get_rating(all_ids[i:i+MAX_RESULTS])['ratings']
             ratings.extend(partratings)
         for rating in ratings:
-            for mkey, mvalue in movies.items():
-                if mvalue['netflix_id'] == rating['href']:
-                    movies[mkey][username] = rating['predicted_rating']
+            for movie in movies:
+                if movie.id == rating['href']:
+                    movie.predictions[user.nickname] = rating['predicted_rating']
     return movies
+
+def create_connections(config):
+    netflix = NetflixAPIV2(config['app_name'],
+                           config['api_key'],
+                           config['api_secret'])
+    users = []
+    for val in config['users'].itervalues():
+        time.sleep(0.1)
+        user = netflix.get_user(val['access_token'],
+                                val['access_token_secret'])
+        for k, v in user.get_details()['user'].items():
+            setattr(user, k, v)
+        users.append(user)
+    return netflix, users
+
+def print_favorites(movies, user):
+    best_rated = sorted(movies,
+                        key=lambda x: x.predictions[user.nickname],
+                        reverse=True)
+    print user.nickname
+    for movie in best_rated:
+        if movie.average_rating != movie.predictions[user.nickname]:
+            print 'Pr %s\tGT %s\tNT %s' % (
+                movie.predictions[user.nickname],
+                movie.gtitle[:20],
+                movie.title['title_short'][:20]
+            )
 
 def main():
     parser = argparse.ArgumentParser()
@@ -104,12 +118,6 @@ def main():
 
     with open('config.json', 'r') as f:
         config = json.loads(f.read())
-    time.sleep(0.1)
-    netflix = NetflixAPIV2(config['app_name'],
-                           config['api_key'],
-                           config['api_secret'])
-
-    # write_histories_to_file(netflix, config['users'])
 
     # account = config['users']['billwanjohi']
     # user = netflix.get_user(account['access_token'],
@@ -119,14 +127,13 @@ def main():
     # for rating in ratings['ratings']:
     #     print "%s : %s" % (rating['title'], rating['predicted_rating'])
 
-    movies = find_a_movie(args.zipcode, netflix, config['users'])
+    netflix, users = create_connections(config)
+    movies = pick_a_movie(args.zipcode, netflix, users)
     # for k, v in movies.items():
     #     if 'billwanjohi' in v:
     #         print 'B %s A %s Name %s' % (round(v['billwanjohi'],1),
     #                                      round(v['average'],1), k)
-    top_picks = sorted(movies.iteritems(), key=['billwanjohi']['predicted_rating'])
-    for i in range(3):
-        print top_picks[i]
+    [print_favorites(movies, user) for user in users]
 
 if __name__ == "__main__":
     sys.exit(main())
